2b15b8f5b10f55d5d6fc908d1388c06e68a1d1db
genthoongyong <yonggenthoong@gmail.com>
2026-09-10 20:32:33 -0600

    chore: add three.js and enable swc minify

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withImages = require('next-images')
 module.exports = withImages({
-    trailingSlash: true,
-    
-  })
+  trailingSlash: true,
+  // three.js ships modern class syntax that Terser (Next 12) can't parse
+  swcMinify: true,
+})
